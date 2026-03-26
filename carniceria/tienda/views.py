e49610f68e5bc0f840f13e307cc1fecb9761ecdc
Che Lopez <jlopezcasemero@gmail.com>
2026-03-25 21:31:54 -0600

    actualizacion 'correcta'

--- carniceria/tienda/views.py
+++ carniceria/tienda/views.py
@@ -19,6 +19,49 @@ def sucursales(request):
 def empresa(request):
     return render(request, 'tienda/empresa.html')
 
-# Si quieres una vista específica de productos
 def productos(request):
     return render(request, 'tienda/productos.html')
+
+    # Simulación de base de datos
+USERS = []
+
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        for user in USERS:
+            if user["username"] == username and user["password"] == password:
+                request.session["user"] = username
+                return redirect("index")
+
+        return render(request, "tienda/login.html", {"error": "Credenciales incorrectas"})
+
+    return render(request, "tienda/login.html")
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Verificar si ya existe
+        for user in USERS:
+            if user["username"] == username:
+                return render(request, "tienda/registro.html", {"error": "Usuario ya existe"})
+
+        USERS.append({
+            "username": username,
+            "password": password
+        })
+
+        return redirect("login")
+
+    return render(request, "tienda/registro.html")
+
+
+def logout_view(request):
+    request.session.flush()
+    return redirect("login")
